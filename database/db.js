2ccb0f9272007b811539e5ec3e80b4dcde9d1da9
Dheekshitha2 <shreedhee0212@gmail.com>
2024-01-02 13:55:40 +0800

    added env variables for database variables

--- database/db.js
+++ database/db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "$hreeDhee212",
-    host: "localhost",
-    port: 5432,
-    database: "loaning_system",
+    user: process.env.DB_USER || "default_user",
+    password: process.env.DB_PASSWORD || "default_password",
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_DATABASE || "loaning_system",
 })
 
 module.exports = pool;
