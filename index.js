3889940c23b163cb33ef9d1dd485ec6e0e358a44
Dheekshitha2 <shreedhee0212@gmail.com>
2024-02-27 13:06:23 +0800

    debugging endpoint

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 app.get('/api/inventory', async (req, res) => {
     try {
         // Adjusted SQL query to only select items where loanable is 'Yes'
-        const allInventory = await pool.query("SELECT * FROM hub_items_unique WHERE loanable = true");
+        const allInventory = await pool.query("SELECT * FROM hub_items_unique WHERE loanable = 'true'");
         res.json(allInventory.rows);
     } catch (err) {
         console.error(err.message);
